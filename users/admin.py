62d208a3c02e6e07d1f25efe872312e15f96ff05
opleiva <uas_ingeco@yahoo.com>
2020-11-23 01:24:04 -0300

    otros

--- users/admin.py
+++ users/admin.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 from.models import Profile
 
 admin.site.register(Profile)
+admin.site.site_header = "Turismo Real - Sistema de Arriendos"
+admin.site.site_title = "Portal Administración - Turismo Real"
+admin.site.index_title = "Bienvenidos al portal de administración"
