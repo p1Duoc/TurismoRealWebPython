2cb8e7c9fcb337c76e931bd97cc9d556889f57c4
opleiva <uas_ingeco@yahoo.com>
2020-11-25 18:39:03 -0300

    cambios varios

--- contacto/views.py
+++ contacto/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from .forms import ContactoForm
 from newsletters.forms import NewsletterForm
 # Create your views here.
@@ -16,8 +18,12 @@ def contacto(request):
             form = NewsletterForm(request.POST)
 
             if form.is_valid():
-                form.save()
-
+                instance = form.save(commit=False)
+                instance.save()
+                messages.success(request, "Enviado Correctamente")
+                return redirect("main:homepage")
+            else:
+                messages.error(request,"No enviado")
     else:
         form = ContactoForm
 
